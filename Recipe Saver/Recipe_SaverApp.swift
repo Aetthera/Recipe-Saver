765bc549151ed9f6463648eec3d3096e613a3930
Alena <alenabelovadmvn@gmail.com>
2023-07-07 17:23:17 -0400

    Initial Commit

---
+++ Recipe Saver/Recipe_SaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_SaverApp.swift
+//  Recipe Saver
+//
+//  Created by Alena Belova  on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_SaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
